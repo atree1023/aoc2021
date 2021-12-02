21876111527464258428380db5428f84af7e0844
atree1023 <64298844+atree1023@users.noreply.github.com>
2021-12-02 00:46:56 +0000

    day 1 problems 1 and 2 complete

---
+++ day1.1/main11.ts
@@ -0,0 +1,9 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./day1.1/input.txt', 'utf8').split('\n').map(Number);
+
+const filterIndex = file.filter(
+  (value, index, array) => array[index] > array[index - 1]
+);
+
+console.log(filterIndex.length);
---
+++ day1.2/main12.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./day1.1/input.txt', 'utf8').split('\n').map(Number);
+
+const rollingSum = file.filter(
+  (value, index, array) =>
+    array[index + 1] + array[index + 2] + array[index + 3] >
+    value + array[index + 1] + array[index + 2]
+);
+
+console.log(rollingSum.length);
