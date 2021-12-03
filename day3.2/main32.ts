ad16d78f41aaba7340da6b0d8a7f15e2ebdffc23
atree1023 <64298844+atree1023@users.noreply.github.com>
2021-12-03 22:49:15 +0000

    day3.2 solution complete

--- day3.2/main32.ts
+++ day3.2/main32.ts
@@ -1,8 +1,40 @@
 import { readFileSync } from 'fs';
 
-const fileToAoA = readFileSync('./input31.txt', 'utf8')
-  .split('\n')
-  .map((value) => {
-    const numArr = value.split('');
-    return numArr.map(Number);
-  });
+const getRating = (inc: number, exc: number): number => {
+  let fileToAoA = readFileSync('./input32.txt', 'utf8')
+    .split('\n')
+    .map((value) => {
+      const numArr = value.split('');
+      return numArr.map(Number);
+    });
+
+  for (let index = 0; index < 12; index++) {
+    let colTotal = 0;
+
+    if (fileToAoA.length === 1) {
+      break;
+    }
+
+    fileToAoA.forEach((value) => {
+      if (value[index] === 1) {
+        colTotal += 1;
+      }
+    });
+
+    if (colTotal >= fileToAoA.length / 2) {
+      fileToAoA = fileToAoA.filter((value) => {
+        return value[index] === inc;
+      });
+    } else {
+      fileToAoA = fileToAoA.filter((value) => {
+        return value[index] === exc;
+      });
+    }
+  }
+  return parseInt(fileToAoA[0].join(''), 2);
+};
+
+const oxygenRating = getRating(1, 0);
+const co2Rating = getRating(0, 1);
+
+console.log(oxygenRating * co2Rating);
