f2972aabb2407f671c8275701a12c30c1f35d9f4
atree1023 <64298844+atree1023@users.noreply.github.com>
2021-12-03 23:13:14 +0000

    format cleanup

--- day1.2/main12.ts
+++ day1.2/main12.ts
@@ -4,8 +4,6 @@ const file = readFileSync('./input12.txt', 'utf8').split('\n').map(Number);
 
 const rollingSum = file.filter(
   (value, index, array) => value < array[index + 3]
-  // array[index + 1] + array[index + 2] + array[index + 3] >
-  // value + array[index + 1] + array[index + 2]
 );
 
 console.log(rollingSum.length);
